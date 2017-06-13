8357b70f5389ebebb258a8d13effc76c1a9dd95c
Brian Best <brian.brianb64@gmail.com>
2017-06-13 07:29:34 -0300

    fixed bug in react router, added search data, created new component for search data

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 // import {  } from 'react-router'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Landing from './landing'
 import Search from './search'
 import '../public/style.css'
@@ -9,12 +9,12 @@ import '../public/style.css'
 const App = React.createClass({
   render () {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div className='app'>
           <Route exact={1} path='/' component={Landing} />
           <Route path='/search' component={Search} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
 
     )
   }
