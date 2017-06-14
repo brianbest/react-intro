7b0150fa79c475b3de5928c25e0dfd8b580b2a25
Brian Best <brian.brianb64@gmail.com>
2017-06-14 19:15:18 -0300

    Added header to search page and made cards do things easier

--- js/search.js
+++ js/search.js
@@ -6,12 +6,19 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} show={show} />
+        <header>
+          <h1>Brian's Video</h1>
+          <input type='text' placeholder='search' />
+        </header>
+        <div>
+          {preload.shows.map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
 
-          )
-        })}
+            )
+          })}
+        </div>
+
       </div>
     )
   }
